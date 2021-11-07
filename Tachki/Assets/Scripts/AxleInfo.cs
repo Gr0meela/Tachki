db8d1a84ced5821c94ac9af12b09e6249dd5f543
Krymov Andrey <black_lord_1999@mail.ru>
2021-11-07 14:14:56 +0700

    Wheel Collider Car
    Машинка на Wheel Collider

---
+++ Tachki/Assets/Scripts/AxleInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
---
+++ Tachki/Assets/Scripts/CarController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private List<AxleInfo> axleInfos; // the information about each individual axle
+    [SerializeField] private float maxMotorTorque; // maximum torque the motor can apply to wheel
+    [SerializeField] private float maxSteeringAngle; // maximum steer angle the wheel can have
+    public void MoveForward()
+    {
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = maxMotorTorque;
+                axleInfo.rightWheel.motorTorque = maxMotorTorque;
+            }
+        }
+    }
+    public void Rotate(float steering)
+    {
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering * maxSteeringAngle;
+                axleInfo.rightWheel.steerAngle = steering * maxSteeringAngle;
+            }
+        }
+    }
+}
