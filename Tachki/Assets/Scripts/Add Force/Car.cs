c7b66f7227084ac0005bb57783174e2188d55b45
Krymov Andrey <black_lord_1999@mail.ru>
2021-11-10 00:55:30 +0700

    Sphere Engine Car
    Машина, работающая через сферу

---
+++ Tachki/Assets/Scripts/Add Force/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public void MoveForward(Transform tr, Rigidbody rb, float speed, RaycastHit hit)
+    {
+        rb.AddForce(tr.right * speed, ForceMode.Acceleration);
+        tr.rotation *= Quaternion.FromToRotation(tr.up, hit.normal);
+    }
+
+    public void Rotate(Transform tr, float steering)
+    {
+        tr.Rotate(0, steering, 0);
+    }
+}
---
+++ Tachki/Assets/Scripts/Add Force/Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    //private bool isAI = false;
+    private bool isGrounded;
+    private Car car;
+    public LayerMask Ground;
+    private Transform car_tr;
+    [SerializeField] private Rigidbody car_rb;
+    [SerializeField] private float max_speed;
+    [SerializeField] private float max_steering;
+    private float steering;
+
+    void Awake()
+    {
+        car = GetComponent<Car>();
+        car_tr = GetComponent<Transform>();
+        car_rb.transform.parent = null;
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        isGrounded = Physics.Raycast(car_tr.position, -car_tr.up, out hit, 1f);
+        Debug.DrawRay(car_tr.position, -car_tr.up, Color.yellow);
+
+        steering = Input.GetAxis("Horizontal") * max_steering;
+
+        car_tr.position = car_rb.transform.position;
+        if (isGrounded)
+        {
+            car.MoveForward(car_tr, car_rb, max_speed, hit);
+            car.Rotate(car_tr, steering);
+        }
+        else
+        {
+            car_rb.AddForce(car_tr.up * -50f);
+        }
+    }
+}
